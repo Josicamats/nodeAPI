cba114916e9d2b0b98530570d61da9f4b082e64a
DESKTOP-BK4NRV9\jose_ <jose_camats_22@hotmail.com>
2021-05-04 23:50:27 -0300

    Mongodb cloud connection created

--- models/connection.js
+++ models/connection.js
@@ -20,7 +20,7 @@ const AccountSchema = new Schema({
 //`mongodb:\/\/${conf.mongo.host}/${conf.mongo.db}`
 //mongoose.connect(`mongodb:\/\/${conf.mongo.host}/${conf.mongo.db}`, { useNewUrlParser: true,  useUnifiedTopology: true });
 
-mongoose.connect(`mongodb:\/\/${process.env.MONGODB_URI}/${process.env.MONGODB_DB}`||`mongodb:\/\/${conf.mongo.host}/${conf.mongo.db}`,
+mongoose.connect(`${process.env.DB_USER}:${process.env.DB_PASS}${process.env.MONGODB_URI}/${process.env.MONGODB_DB}?retryWrites=true&w=majority`||`mongodb:\/\/${conf.mongo.host}/${conf.mongo.db}`,
 { useNewUrlParser: true , useUnifiedTopology: true })
 .then(()=>{ return console.log(`Connected to database:${conf.mongo.db} on host: ${conf.mongo.host}`)
  })
